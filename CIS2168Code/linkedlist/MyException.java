666ad81b734feaaccaf64073cc4acd0c0fe1f475
FinalF <flameyufeng@gmail.com>
2013-10-17 00:29:20 -0400

    write own single-linked list

---
+++ CIS2168Code/linkedlist/MyException.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package linkedlist;
+
+/**
+ *
+ * @author wang
+ */
+public class MyException extends Exception  
+{  
+    public MyException(String message)          
+    {   
+        super(message);         
+    }       
+}
